7fed7dc6685e825a5aa21b97bacf3057c9bbdb04
Anastasiya-Ivanova97 <55584833+Anastasiya-Ivanova97@users.noreply.github.com>
2019-10-13 00:24:44 +0300

    Update CucumberRunner.java

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -4,7 +4,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
-@Ignore
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
