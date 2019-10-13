ba484361f5a15e75bcd41ce0c58b601a2f7b00eb
Anastasiya-Ivanova97 <55584833+Anastasiya-Ivanova97@users.noreply.github.com>
2019-10-14 01:25:09 +0300

    Update StartPage.java

--- src/main/java/ru/aplana/autotest/pages/StartPage.java
+++ src/main/java/ru/aplana/autotest/pages/StartPage.java
@@ -1,42 +1,35 @@
 package ru.aplana.autotest.pages;
 
 import cucumber.api.java.en.When;
+import io.cucumber.java.en.Then;
 import io.qameta.allure.Step;
+import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import ru.aplana.autotest.steps.Settings;
-
-import java.util.function.Function;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class StartPage extends BasePage{
-    static {
-        Settings.init();
-    }
 
-    ////ul[@class='lg-menu__list']/descendant::span[text()='Ипотека']/ancestor::li/descendant::ul[@class='lg-menu__sub-list']/descendant::a[text()='Ипотека на готовое жильё']
 
     @FindBy(xpath = "//ul[@class='lg-menu__list']")
     WebElement menuBox;
 
-    @Test
-    @Step
-    @When("пользователь наводит курсор на элемент Ипотека верхнего меню")
+    @Then("^пользователь выбирает элемент верхнего меню с параметром \"(.+)\"")
+    @Step("Пользователь выбирает элемент верхнего меню с параметром String {menuItem}")
     public void hoverOnElement(String menuItem) {
+        wait.until(ExpectedConditions.visibilityOf(Settings.driver.findElement(By.xpath("//ul[@class='lg-menu__list']"))));
         Actions builder = new Actions(Settings.getDriver());
-        element = menuBox.findElement(By.xpath("//ul[@class='lg-menu__list']/descendant::span[text()='"+menuItem+"']"));
+        element = Settings.driver.findElement(By.xpath("//ul[@class='lg-menu__list']/descendant::span[text()='"+menuItem+"']"));
         builder.moveToElement(element).build().perform();
     }
-    @Step
-    @When("Когда пользователь выбирает элемент Ипотека на готовое жилье из открывшегося меню")
+    @When("^Пользователь выбирает подменю с параметром String \"(.+)\"")
+    @Step ("Пользователь выбирает подменю с параметром {subMenuItem}")
     public void chooseSubMenuItem(String subMenuItem) {
         WebElement newElement = element.findElement(By.xpath("//ul[@aria-label='Подменю']"));
         element = newElement.findElement(By.xpath("//ul[@aria-label='Подменю']/descendant::a[text()='"+subMenuItem+"']"));
         element.click();
-
     }
 
 }
