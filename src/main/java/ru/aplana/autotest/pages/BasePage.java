73793f8e50c259f0b4fcee6f5b11bd439a10bf67
Anastasiya-Ivanova97 <55584833+Anastasiya-Ivanova97@users.noreply.github.com>
2019-10-14 12:49:05 +0300

    Update BasePage.java

--- src/main/java/ru/aplana/autotest/pages/BasePage.java
+++ src/main/java/ru/aplana/autotest/pages/BasePage.java
@@ -3,11 +3,9 @@ package ru.aplana.autotest.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import ru.aplana.autotest.steps.Settings;
 
 
-
-import static ru.aplana.autotest.steps.Settings.driver;
+import static ru.aplana.autotest.pages.Settings.driver;
 
 public class BasePage {
     public WebElement element;
